b6e189df83e311a9fcc7fbbe1435c1b860d4e818
Kelvin Wise <amenkelvin@gmail.com>
2023-07-17 10:43:30 +0100

    Our First commit, login integrate

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    @include('layouts.partials.head')
+</head>
+<body>
+    @yield('content')
+
+    @include('layouts.partials.scripts')
+</body>
+</html>
